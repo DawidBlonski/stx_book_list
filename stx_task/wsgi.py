9dd93eb55c06c8df133edaad2fce86383863cde8
Dawid <davidblonsky88@gmail.com>
2020-10-17 16:34:00 +0200

    Add postgre config and change folder name

--- stx_task/wsgi.py
+++ stx_task/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for stx project.
+WSGI config for stx_task project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stx.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stx_task.settings')
+print('a')
 application = get_wsgi_application()
