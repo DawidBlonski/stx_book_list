662b73b39b7811f0865772cb523d4831f2398e3b
Dawid <davidblonsky88@gmail.com>
2020-10-18 16:59:26 +0200

    create models

--- stx_task/wsgi.py
+++ stx_task/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stx_task.settings')
-print('a')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stx_task.settings")
+print("a")
 application = get_wsgi_application()
