a78b9d16822d9e06b7ec9d6f31b13b48f3285f1d
Dawid <davidblonsky88@gmail.com>
2020-10-18 17:16:57 +0200

    Add author model test

--- book_list/tests.py
+++ book_list/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Author,Book
 
-# Create your tests here.
+class AuthorTest(TestCase):
+
+    def create_author(self,name='Autor'):
+            return Author.objects.create(name=name)
+
+    def test_author(self):
+        author = self.create_author()
+        self.assertTrue(isinstance(author,Author))
+        self.assertEqual(author.__str__(),author.name)
+        self.assertEqual('Autor',author.name)
+
+class BookTest(TestCase):
+
+    def create_book(self,name='Autor'):
+            return Author.objects.create(name=name)
+
+    def test_author(self):
+        author = self.create_author()
+        self.assertTrue(isinstance(author,Author))
+        self.assertEqual(author.__str__(),author.name)
+        self.assertEqual('Autor',author.name)
