64b42511bc092638d223f9f4772d9f57a75cba92
Dawid <davidblonsky88@gmail.com>
2020-10-21 01:55:39 +0200

    add drf

--- book_list/filters.py
+++ book_list/filters.py
@@ -6,10 +6,10 @@ class BookFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr="icontains", field_name="title")
     language = django_filters.CharFilter(lookup_expr="icontains", field_name="language")
     published_date__gt = django_filters.NumberFilter(
-        field_name="published_date", lookup_expr="year__gt"
+        field_name="published_date", lookup_expr="gt"
     )
     published_date__lt = django_filters.NumberFilter(
-        field_name="published_date", lookup_expr="year__lt"
+        field_name="published_date", lookup_expr="lt"
     )
 
     class Meta:
--- book_list/google_api.py
+++ book_list/google_api.py
@@ -20,8 +20,9 @@ def create_query(key_words, title, author):
 
 
 def create_published_date(published_date):
-    year = re.search(r"\d{4}", published_date).group()
-    return datetime.datetime(year=int(year), month=1, day=1)
+    if published_date:
+        year = re.search(r"\d{4}", published_date).group()
+        return int(year)
 
 
 def authors_to_string(authors):
@@ -30,7 +31,7 @@ def authors_to_string(authors):
 
 
 def get_isbn(isbns):
-    next(
+    return next(
         (isbn.get("identifier", "") for isbn in isbns if isbn.get("type") == "ISBN_13"),
         None,
     )
@@ -54,26 +55,17 @@ def get_books(key_words, title=None, author=None):
         isbn_numbers = volume_info.get("industryIdentifiers", {})
         language = volume_info.get("language", None)
         isbn_number = get_isbn(isbn_numbers)
+        print(isbn_number)
         thumbnail = volume_info.get("imageLinks", {}).get("thumbnail", None)
-        yield title, authors, published_date, isbn_number, page_count, thumbnail, language
-
+        yield {
+            "title":title,
+            "authors":authors,
+            "published_date":published_date,
+            "page_count":page_count,
+            "language":language,
+            "isbn_number":isbn_number,
+            "thumbnail":thumbnail
+        }
 
 def books_to_database(key_words, title=None, author=None):
-    for (
-        title,
-        authors,
-        published_date,
-        isbn_number,
-        page_count,
-        thumbnail,
-        language,
-    ) in get_books(key_words, title, author):
-        Book.objects.create(
-            title=title,
-            authors=author,
-            published_date=published_date,
-            isbn_number=isbn_number,
-            page_count=page_count,
-            thumbnail=thumbnail,
-            language=language,
-        )
+    Book.objects.bulk_create([Book(**i) for i in get_books(key_words, title, author)])
